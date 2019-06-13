ee04d0604b12a5ec8aeaa97f9ae5ac10eda7d1a0
fvasyliuk <fedron18@gmail.com>
2019-06-14 00:42:31 +0300

    api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const createProxy = require('http-proxy-middleware');
 const proxy = createProxy({
     target: 'https://apiko-marketplace-api-2019.herokuapp.com/',
     pathRewrite: {
-        '^intense-earth-67837.herokuapp.com/api': '',
+        '^https://intense-earth-67837.herokuapp.com/api': '',
     },
     changeOrigin: true,
 });
